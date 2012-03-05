44d20f957fb5284e827920b813503a00b5f7c086
jculak <jculak@u.northwestern.edu>
2012-03-05 16:20:02 -0600

    Expanded a little

--- twitter/main.py
+++ twitter/main.py
@@ -6,15 +6,26 @@ def main():
     api = twitter.Api()
     candidates = [("romney", 0), ("paul",0), ("gingrich",0), ("santorum",0)]
     searchterm = "#supertuesday"
-    print "Searching twitter:\n"
-    results = api.GetSearch(searchterm,per_page=100)
+    results = []
     
+    print "Searching twitter:\n"
+    results = getSearches(api, searchterm, 1050)
     for status in results:
         for i in range(len(candidates)):
             if candidates[i][0] in status.text.lower():
                 candidates[i] = (candidates[i][0], candidates[i][1] +1)
     print candidates
 
+def getSearches(api, searchterm, num):
+    """ Search twitter for num searches using searchterm """
+    results = []
+    while num > 100:
+        results.extend(api.GetSearch(searchterm,per_page=100))
+        num = num - 100
+    results.extend(api.GetSearch(searchterm,per_page=num))
+    return results
+
+    
 
 if __name__ == "__main__":
     main()                
