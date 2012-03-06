e5ebd33ba626f76c2ea247539d33d4fa51974f81
Christina Burghard <burghard.christina@gmail.com>
2012-03-06 00:31:33 -0600

    sentiment outputs a description

--- twitter/sentiment.py
+++ twitter/sentiment.py
@@ -7,19 +7,29 @@ def getSentiment(text):
         data="text=%s" % text).read())
 
 def allSentiments(results):   
-    allTweets = results' '.join(results)
+    allTweets = ' '.join(results)
     sentVal = 0
+    rating = 0
     text = unicodedata.normalize('NFKD', allTweets).encode('ascii','ignore')
     
     sentDict = getSentiment(text)
     sent = sentDict['label']
-    
-    if sent == 'pos':
-        sentVal = 1
-       # posTweets.add(text)
+   
+    if sent ==  'pos':
+        rating = sentDict['probability']['pos']
+        if rating > .75: 
+            sentiment = "strongly positive"
+        else:
+            sentiment = "slightly positive"
+        
     elif sent == 'neg':
-        sentVal = -1
-       # negTweets.add(text)
+        rating = sentDict['probability']['neg']   
+        if rating > .75:
+            sentiment = "strongly negative"
+        else:
+            sentiment = "slightly negative"
+    else:    
+        sentiment = "neutral"
      
     return sentVal
  
